80e51a81a56f39dcd1fb0a00f9c4d5f3165a4b06
MarcoAntonioNavaAburto <141948018+MarcoAntonioNavaAburto@users.noreply.github.com>
2023-10-15 22:00:52 -0600

    Add files via upload

---
+++ Producto.java
@@ -0,0 +1,28 @@
+package ejemplotaqueria;
+import java.io.Serializable;
+
+public class Producto implements Serializable {
+    private String nombre;
+    private double precio;
+    private String descripcion;
+
+    public Producto(String nombre, double precio, String descripcion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    // Getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
